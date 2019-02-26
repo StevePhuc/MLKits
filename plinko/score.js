2dc9529e01ac2fc196c5e6a527b5bcbac80b96d9
Steve <contact@phuc.fi>
2019-02-26 05:32:09 +0200

    recording data

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,11 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+    // Ran every time a balls drops into a bucket
+    output.push([dropPosition, bounciness, size, bucketLabel]);
+    console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+    // Write code here to analyze stuff
 }
-
